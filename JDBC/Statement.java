ee3f37af348bb2cb5973ee60a81768053bc466cc
Rohit Mittal <35946128+rohitm17@users.noreply.github.com>
2020-03-04 16:57:52 +0530

    for statement

---
+++ JDBC/Statement.java
@@ -0,0 +1,14 @@
+import java.sql.*;  
+class first{  
+public static void main(String args[]){  
+try{  
+Class.forName("com.mysql.jdbc.Driver");  
+Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/demo","student","student");  
+Statement stmt=con.createStatement();  
+ResultSet rs=stmt.executeQuery("select * from employees");  
+while(rs.next())  
+System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
+con.close();  
+}catch(Exception e){ System.out.println(e);}  
+}  
+}
